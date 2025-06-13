7e256f8da8ff80c0d4d4c935b15b921779cb545a
federicobianucci <federicobianucci@gmail.com>
2025-06-13 09:37:06 +0200

    first commit

---
+++ src/friday-quiz-1/libraries/constants/Lib_PredeployAddresses.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+library Lib_PredeployAddresses {
+    address internal constant L2_CROSS_DOMAIN_MESSENGER = 0x4200000000000000000000000000000e1ec715ec;
+    address internal constant L2_ERC20_BRIDGE = 0x4200000000000000000000000000000000f21da7;
+    address internal constant L2_WETH = payable(0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000);
+}
